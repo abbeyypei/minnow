d7766c7000f6b638feaeb52ac46b64d1021c06ae
abbeyypei <abbeybae1205@gmail.com>
2023-05-01 00:21:45 +0000

    minor

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -27,10 +27,8 @@ uint64_t TCPSender::consecutive_retransmissions() const
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
   // Your code here.
-  // cout << "maybe send retrans" << retransmit_ << "\n";
   if (retransmit_ && !retransmit_queue_.empty()) {
     TCPSenderMessage msg = retransmit_queue_.top().second;
-    // cout << "retrans msg " << retransmit_queue_.top().first << "\n";
     retrans_timer_ = cur_time_;
     retransmit_ = false;
     return msg;
@@ -50,7 +48,6 @@ optional<TCPSenderMessage> TCPSender::maybe_send()
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // cout << "push called with window " << window_ << "\n";
   // Your code here.
   string payload = "";
   TCPSenderMessage msg;
@@ -67,7 +64,6 @@ void TCPSender::push( Reader& outbound_stream )
       payload,
       fin
     };
-    cout << "pushing SYN \n";
     msg_queue_.push(make_pair(isn_, msg));
     seq_num_in_flight_ += msg.sequence_length();
     isn_ = isn_ + msg.sequence_length();
@@ -78,7 +74,6 @@ void TCPSender::push( Reader& outbound_stream )
   {
     uint64_t payload_size = min(outbound_stream.bytes_buffered(), TCPConfig::MAX_PAYLOAD_SIZE);
     payload_size = min(payload_size, static_cast<uint64_t>(window_ - isn_));
-    // cout << "payload size " << payload_size << "\n";
     read(outbound_stream, payload_size, payload);
     if (outbound_stream.is_finished() && isn_ + payload.size() < window_) {
       fin = true;
@@ -90,7 +85,6 @@ void TCPSender::push( Reader& outbound_stream )
       payload,
       fin
     };
-    // cout << "pushing " << payload << "\n";
     msg_queue_.push(make_pair(isn_, msg));
     isn_ = isn_ + msg.sequence_length();
     seq_num_in_flight_ += msg.sequence_length();
@@ -104,7 +98,6 @@ void TCPSender::push( Reader& outbound_stream )
       payload,
       true
     };
-    // cout << "pushing FIN \n";
     msg_queue_.push(make_pair(isn_, msg));
     seq_num_in_flight_ += msg.sequence_length();
     isn_ = isn_ + msg.sequence_length();
@@ -116,7 +109,6 @@ void TCPSender::push( Reader& outbound_stream )
 TCPSenderMessage TCPSender::send_empty_message()
 {
   // Your code here.
-  // cout << "empty message \n";
   string payload = "";
   TCPSenderMessage msg = {
       isn_,
@@ -124,7 +116,6 @@ TCPSenderMessage TCPSender::send_empty_message()
       payload,
       false
     };
-  // isn_ = isn_ + 1;
   return msg;
 }
 
@@ -176,7 +167,6 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
   // Your code here.
-  // cout << "1 - retrans " << retrans_timer_ << " curtime " << cur_time_ << " rto " << rto_ << " window " << window_ << " isn " << isn_ <<  "\n";
   (void)ms_since_last_tick;
   cur_time_ += ms_since_last_tick;
   if (retrans_timer_ > 0  && retrans_timer_ + rto_ <= cur_time_) {
@@ -184,12 +174,10 @@ void TCPSender::tick( const size_t ms_since_last_tick )
     if (((!zero_window_) && isn_ < window_ + 1)) {
         rto_ *= 2;
         cons_retrans_ += 1;
-        // retransmit_ = false;
     }
     retrans_timer_ = cur_time_;
   } else {
     retransmit_ = false;
   }
   
-  // cout << "2 - retrans " << retrans_timer_ << " curtime " << cur_time_ << " rto " << rto_ << " window " << window_ << " cons retrans " << cons_retrans_ << "\n";
 }
